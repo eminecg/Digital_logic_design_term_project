463f03189f1e1b6649733b1d0f6856d5263abd44
emnc <emine.cig67@gmail.com>
2021-12-23 22:41:46 +0300

    some of methods added

--- src/main.java
+++ src/main.java
@@ -29,88 +29,164 @@ public class main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-      String inputFilePath ="input.txt";
-      //String myOpcodeList[][]=new String[20][4];  // Bir register en fazla kaç değişken içeriyor ?
+        String inputFilePath = "input.txt";
+
         ArrayList<ArrayList<String>> myOpcodeList = new ArrayList<ArrayList<String>>();
 
-      //File file =new File(inputFilePath);
-        FileInputStream fis=new FileInputStream(inputFilePath);
-        Scanner sc=new Scanner(fis);    //file to be scanned
+        //File file =new File(inputFilePath);
+        FileInputStream fis = new FileInputStream(inputFilePath);
+        Scanner sc = new Scanner(fis);    //file to be scanned
 
-        int instIndex=0;
-        while(sc.hasNextLine())
-        {
-           // System.out.println(sc.nextLine());
-            // each line instruction exp ( 'LD' , 'R1,1' )
-            String instructionSet[]=sc.nextLine().split("[ ,]+");
+        int instIndex = 0;
+        while (sc.hasNextLine()) {
+            String instructionSet[] = sc.nextLine().split("[ ,]+");
 
-            ArrayList<String> temp= new ArrayList<String>();
-            for (int i=0; i<instructionSet.length; i++) {
+            ArrayList<String> temp = new ArrayList<String>();
+            for ( int i = 0; i < instructionSet.length; i++ ) {
 
                 temp.add(instructionSet[i]);
             }
-                myOpcodeList.add(temp);
-
-
-
+            myOpcodeList.add(temp);
             instIndex++;
         }
         sc.close();     //closes the scanner
 
         // print
-        for (int i=0; i<myOpcodeList.size(); i++){
-            for (int j=0; j<myOpcodeList.get(i).size(); j++){
+        for ( int i = 0; i < myOpcodeList.size(); i++ ) {
+            for ( int j = 0; j < myOpcodeList.get(i).size(); j++ ) {
                 System.out.println(myOpcodeList.get(i).get(j));
             }
         }
+        String dest;
+        String src1;
+        String src2;
+        String imm;
+        String opp1;
+        String opp2;
+        String addr;
 
-        // determine opcode type
-        System.out.println("--");
-        System.out.println(myOpcodeList.get(0).get(0));
-        System.out.println(opcodeList[8][0]);
-        System.out.println(opcodeList[8][1]);
-        System.out.println(myOpcodeList.size());
-        for (int i=0; i<myOpcodeList.size(); i++){
-            if (myOpcodeList.get(i).get(0).equals (opcodeList[0][0]) ) // AND
-                System.out.println(opcodeList[0][1]);
-            else if (myOpcodeList.get(i).get(0).equals (opcodeList[1][0]) )   // ANDI
-                System.out.println(opcodeList[1][1]);
-            else if (myOpcodeList.get(i).get(0).equals (opcodeList[2][0]) )   // ADD
-                System.out.println(opcodeList[2][1]);
-            else if (myOpcodeList.get(i).get(0).equals (opcodeList[3][0]) ) // ADDI
-                System.out.println(opcodeList[3][1]);
-            else if(myOpcodeList.get(i).get(0).equals (opcodeList[4][0]) ) //  OR
-                System.out.println(opcodeList[4][1]);
-            else if (myOpcodeList.get(i).get(0) .equals (opcodeList[5][0]) ) //  ORI
+
+        for ( int i = 0; i < myOpcodeList.size(); i++ ) {
+            if (myOpcodeList.get(i).get(0).equals(opcodeList[0][0])) {// AND
+                dest = myOpcodeList.get(i).get(1);
+                src1 = myOpcodeList.get(i).get(2);
+                src2 = myOpcodeList.get(i).get(3);
+
+                  andFuncForm(opcodeList[0][1],dest,src1,src2);
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[1][0])) {   // ANDI
+                dest = myOpcodeList.get(i).get(1);
+                src1 = myOpcodeList.get(i).get(2);
+                imm = myOpcodeList.get(i).get(3);
+
+                andIFuncForm(opcodeList[1][1],dest,src1,imm);
+
+
+
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[2][0])) { // ADD
+
+                dest = myOpcodeList.get(i).get(1);
+                src1 = myOpcodeList.get(i).get(2);
+                src2 = myOpcodeList.get(i).get(3);
+
+                andFuncForm(opcodeList[2][1], dest, src1, src2);
+
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[3][0])) {// ADDI
+                dest = myOpcodeList.get(i).get(1);
+                src1 = myOpcodeList.get(i).get(2);
+                imm = myOpcodeList.get(i).get(3);
+
+                andIFuncForm(opcodeList[3][1],dest,src1,imm);
+
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[4][0])) {//  OR
+
+                dest = myOpcodeList.get(i).get(1);
+                src1 = myOpcodeList.get(i).get(2);
+                src2 = myOpcodeList.get(i).get(3);
+
+                andFuncForm(opcodeList[4][1], dest, src1, src2);
+
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[5][0])) {//  ORI
                 System.out.println(opcodeList[5][1]);
-            else if (myOpcodeList.get(i).get(0).equals (opcodeList[6][0]) )//  XOR
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[6][0])) {  //  XOR
                 System.out.println(opcodeList[6][1]);
-            else if (myOpcodeList.get(i).get(0).equals (opcodeList[7][0]) ) // XORI
-                System.out.println(opcodeList[7][1]);
-            else if (myOpcodeList.get(i).get(0).equals( opcodeList[8][0]) ) // LD
-                System.out.println(opcodeList[8][1]);
-            else if (myOpcodeList.get(i).get(0) .equals (opcodeList[9][0]) ) // ST
-                System.out.println(opcodeList[9][1]);
-            else if (myOpcodeList.get(i).get(0).equals (opcodeList[10][0]) )  // JUMP
-                System.out.println(opcodeList[10][1]);
-            else if (myOpcodeList.get(i).get(0).equals (opcodeList[11][0]) ) //  BEQ
-                System.out.println(opcodeList[11][1]);
-            else if (myOpcodeList.get(i).get(0).equals (opcodeList[12][0]) )//  BGT
-                System.out.println(opcodeList[12][1]);
-            else if (myOpcodeList.get(i).get(0).equals (opcodeList[13][0]) )  // BLT
-                System.out.println(opcodeList[13][1]);
-            else if (myOpcodeList.get(i).get(0) .equals (opcodeList[14][0]) )  // BGE
-                System.out.println(opcodeList[14][1]);
-            else if (myOpcodeList.get(i).get(0).equals (opcodeList[15][0]) ) // BLE
-                System.out.println(opcodeList[15][1]);
-            else
-                System.out.println("Please enter correct input.");
 
+                dest = myOpcodeList.get(i).get(1);
+                src1 = myOpcodeList.get(i).get(2);
+                src2 = myOpcodeList.get(i).get(3);
+
+                andFuncForm(opcodeList[6][1], dest, src1, src2);
+
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[7][0])) {   // XORI
+                System.out.println(opcodeList[7][1]);
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[8][0])) { // LD
+
+
+                dest = myOpcodeList.get(i).get(1);
+                addr = myOpcodeList.get(i).get(2);
+
+
+                if (Integer.parseInt(addr) < 0) {
+                    System.out.println("Address for LD must be greater than 0 !");
+                    break;
+
+
+                }
+                if ( Integer.parseInt(addr) > 1024){  // 2'10
+                    System.out.println("Address can\'t be greater than 256 for LD !");
+                    break;
+                }
+
+            // eksik
+
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[9][0])) {// ST
+                System.out.println(opcodeList[9][1]);
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[10][0])) {  // JUMP
+                System.out.println(opcodeList[10][1]);
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[11][0])) {  //  BEQ
+                System.out.println(opcodeList[11][1]);
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[12][0])) {  //  BGT
+                System.out.println(opcodeList[12][1]);
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[13][0])) {   // BLT
+                System.out.println(opcodeList[13][1]);
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[14][0])) {    // BGE
+                System.out.println(opcodeList[14][1]);
+            } else if (myOpcodeList.get(i).get(0).equals(opcodeList[15][0])) {// BLE
+                System.out.println(opcodeList[15][1]);
+            } else {
+                System.out.println("Please enter correct input.");
+            }
         }
 
 
-
     }
+
+
+    static String  andFuncForm(String operation,String dest,String src1,String src2){
+        if (operation.equals("ADD") || operation.equals( "AND")){
+
+           // binary_register_values = self.__convert_registers_to_binary(dest = dest, src_1 = src_1, src_2 = src_2)
+
+            //binary_code += ''.join(binary_register_values)
+        }
+        return " ";
+    }
+
+    static String  andIFuncForm(String operation,String dest,String src1,String imm){
+        if (operation.equals("ADD") || operation.equals( "AND")){
+
+            // binary_register_values = self.__convert_registers_to_binary(dest = dest, src_1 = src_1, src_2 = src_2)
+
+            //binary_code += ''.join(binary_register_values)
+        }
+        return " ";
+    }
+
+    static String  convertRegistersToBinary(){
+
+
+        return "";
+    }
+
 
 
 }
